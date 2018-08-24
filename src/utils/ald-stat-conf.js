868018eee7c5d5d40deb8d38ce0c9a7fdc4b0e39
manbasji <276066021@qq.com>
2018-08-24 18:23:30 +0800

    u

---
+++ src/utils/ald-stat-conf.js
@@ -0,0 +1,3 @@
+exports.app_key = "41af47c820f0f130f344c2c5142f7c1f"; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = true; //默认不获取用户坐标位置
+exports.plugin = false;  //您的小程序中是否使用了插件。根据是否启用插件会有不同的接入方式，请参考文档文档。
